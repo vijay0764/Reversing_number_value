ad8a8324c3d1b4e7aa71d8081006dcfece4fc2c3
vijay0764 <90964627+vijay0764@users.noreply.github.com>
2021-09-22 17:05:23 +0530

    Update reversing_number.cpp

--- reversing_number.cpp
+++ reversing_number.cpp
@@ -5,14 +5,15 @@ using namespace std;
 int main()
 {
     int n;
-    cin >> n;
+    cin >> n;                   //Taking input 
     int m;
-    do{
+    do{                                     //modulus to print last digit -- eg.5864 gives 4
+        
          m=n%10;
          cout << m;
-         n=n/10;
+         n=n/10;                                   // division delete the last digit-- eg. 5864 becomes 586
     }
-    while(n/10!=0 || n%10!=0);
+    while(n/10!=0 || n%10!=0);                  // it repeat the process this either it given no. becomes less then 0 or last digits is 0 
 
 return 0;
 }
